2c3d4b837156adf7ae21d5dab81326347ca86693
Steven Fröhlich <steven.froehlich@valtech.com>
2021-11-09 10:04:38 +0100

    basic contact form

--- src/components/areaContact/AreaContact.js
+++ src/components/areaContact/AreaContact.js
@@ -5,9 +5,31 @@ import "./areaContact.scss";
 
 function AreaContact() {
   return (
-    <div className="areaContact" id="areaContact">
-      <ScrollToTop />
-    </div>
+    <>
+      <div className="areaContact" id="areaContact">
+        <form>
+          <label htmlFor="name">
+            <input type="text" id="name" placeholder="Name" />
+            <span>Name</span>
+          </label>
+          <label htmlFor="email">
+            <input type="email" id="email" placeholder="Email" />
+            <span>Email</span>
+          </label>
+          <label htmlFor="concerning">
+            <input type="text" id="concerning" placeholder="Concerning" />
+            <span>Concerning</span>
+          </label>
+          <label htmlFor="message">
+            <textarea type="text" id="message" placeholder="Message" />
+            <span>Message</span>
+          </label>
+
+          <button type="button">SEND</button>
+        </form>
+        <ScrollToTop />
+      </div>
+    </>
   );
 }
 
