1c49c1c61fbb60ac0be419058a726b45708e399e
Steven Fröhlich <steven.froehlich@valtech.com>
2021-11-09 15:03:15 +0100

    setting media query

--- src/components/areaContact/AreaContact.js
+++ src/components/areaContact/AreaContact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ScrollToTop from "../scrollToTop/ScrollToTop";
 
 import "./areaContact.scss";
@@ -7,27 +7,46 @@ function AreaContact() {
   return (
     <>
       <div className="areaContact" id="areaContact">
-        <form>
-          <label htmlFor="name">
-            <input type="text" id="name" placeholder="Name" />
-            <span>Name</span>
-          </label>
-          <label htmlFor="email">
-            <input type="email" id="email" placeholder="Email" />
-            <span>Email</span>
-          </label>
-          <label htmlFor="concerning">
-            <input type="text" id="concerning" placeholder="Concerning" />
-            <span>Concerning</span>
-          </label>
-          <label htmlFor="message">
-            <textarea type="text" id="message" placeholder="Message" />
-            <span>Message</span>
-          </label>
-
-          <button type="button">SEND</button>
-        </form>
-        <ScrollToTop />
+        <div className="wrapperArea">
+          <div className="wrapperHeading">
+            <h2>CONTACT US</h2>
+            <p>We take time for your questions.</p>
+            <p>And are happy to show you a product demo.</p>
+          </div>
+          <div className="wrapperForm">
+            <form
+              action="https://formsubmit.co/info@downsiteup.de"
+              method="POST"
+            >
+              <label htmlFor="name">
+                <input type="text" id="name" placeholder="Name" />
+                <span>Name</span>
+              </label>
+              <label htmlFor="email">
+                <input type="email" id="email" placeholder="Email" />
+                <span>Email</span>
+              </label>
+              <label htmlFor="subject">
+                <input type="text" id="subject" placeholder="Subject" />
+                <span>Subject</span>
+              </label>
+              <label htmlFor="message">
+                <textarea
+                  type="text"
+                  id="message"
+                  placeholder="Message"
+                  row="50"
+                  columns="50"
+                />
+                <span>Message</span>
+              </label>
+              <input type="checkbox" />
+              <p>Agree</p>
+              <button type="submit">SEND</button>
+            </form>
+            <ScrollToTop />
+          </div>
+        </div>
       </div>
     </>
   );
