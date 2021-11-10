b18f541d985979c90576df7cbb8bc486f849ef66
Steven Fröhlich <steven.froehlich@valtech.com>
2021-11-10 12:02:58 +0100

    including meta data favicon

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './footer.scss';
+import React from "react";
+import "./footer.scss";
+import logoWhite from "../../assets/img/logoWhite.svg";
 
 function Footer() {
-    return (
-        <div className="footer" id="footer">
-            
-        </div>
-    )
+  return (
+    <div className="footer" id="footer">
+      <a href="/">Privacy Policy</a>
+      <a href="/">Impressum</a>
+      <img src={logoWhite} alt="exact mobility logo" />
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
